c3c3fe9f1114e2b95b314c43139b7f0a3d811201
Joel Bernstein <jbernste@apache.org>
2022-07-29 10:06:50 -0400

    WIP

--- solr/modules/external-files/src/java/org/apache/solr/util/external/ExternalFileUtil.java
+++ solr/modules/external-files/src/java/org/apache/solr/util/external/ExternalFileUtil.java
@@ -20,15 +20,19 @@ package org.apache.solr.util.external;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
 import org.apache.solr.common.cloud.DocCollection;
 import org.apache.solr.common.cloud.DocRouter;
+import org.apache.solr.common.cloud.Slice;
 import org.apache.solr.common.util.Hash;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class ExternalFileUtil {
 
-  private static int HASH_SEED = 12131344;
+  private static final int HASH_SEED = 12131344;
+  private static final int HASH_DIRS = 250;
+  private static final String SHARD_TEMP_FILE = "temp.bin";
+  private static final int NUM_PARTITIONS = 11;
+
 
   public static void main(String[] args) throws Exception{
 
@@ -58,11 +62,127 @@ public class ExternalFileUtil {
     return null;
   }
 
-  public static void process(ExternalFile externalFile, String outRoot, CloudSolrClient cloudSolrClient, String mainCollection) {
+  public static void process(ExternalFile externalFile, String outRoot, CloudSolrClient cloudSolrClient, String mainCollection) throws IOException {
+
+    if(oldData(externalFile, outRoot)) {
+      return;
+    }
+
     DocCollection docCollection = cloudSolrClient.getClusterState().getCollection(mainCollection);
     DocRouter docRouter = docCollection.getRouter();
+    BufferedReader in = null;
+    //Split the file by shard.
+    Map<String, DataOutputStream> shardOuts = new HashMap<>();
+    List<File> shardHomes = new ArrayList<>();
+    try {
+      in = new BufferedReader(new FileReader(externalFile.file));
+      String line = null;
+      while((line = in.readLine()) != null) {
+        String[] pair = line.split(":");
+        String id = pair[0].trim();
+        float f = Float.parseFloat(pair[1]);
+        Slice slice = docRouter.getTargetSlice(id, null, null, null, docCollection);
+        String shardId = slice.getName();
+        if(shardOuts.containsKey(shardId)) {
+          DataOutputStream shardOut = shardOuts.get(shardId);
+          byte[] bytes = id.getBytes();
+          shardOut.writeByte(bytes.length);
+          shardOut.write(bytes);
+          shardOut.writeFloat(f);
+        } else {
+          DataOutputStream shardOut = openShardOut(outRoot, externalFile, shardId, SHARD_TEMP_FILE);
+          shardHomes.add(getShardHome(outRoot, externalFile, shardId));
+          byte[] bytes = id.getBytes();
+          shardOut.writeByte(bytes.length);
+          shardOut.write(bytes);
+          shardOut.writeFloat(f);
+          shardOuts.put(shardId, shardOut);
+        }
+      }
+    } finally {
+      in.close();
+      for(DataOutputStream dataOutputStream : shardOuts.values()) {
+        dataOutputStream.close();
+      }
+    }
+    partitionShards(shardHomes);
 
+  }
 
+  public static void partitionShards(List<File> shardHomes) throws IOException {
+    //Process the shard files
+    byte[] bytes = new byte[128];
+
+    for(File shardHome : shardHomes) {
+      DataOutputStream[] partitions = new DataOutputStream[NUM_PARTITIONS];
+      DataInputStream tempStream = null;
+      File tempFile = new File(shardHome, SHARD_TEMP_FILE);
+      try {
+        tempStream = new DataInputStream(new BufferedInputStream(new FileInputStream(tempFile)));
+        while (true) {
+          byte b = tempStream.readByte();
+          tempStream.read(bytes, 0, b);
+          float f = tempStream.readFloat();
+          int hash = Hash.murmurhash3_x86_32(bytes, 0, b, HASH_SEED);
+          int bucket = hash % partitions.length;
+          if(partitions[bucket] == null) {
+            partitions[bucket] = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(new File(shardHome, SHARD_TEMP_FILE+"."+bucket))));
+          }
+
+          partitions[bucket].writeByte(b);
+          partitions[bucket].write(bytes, 0, b);
+          partitions[bucket].writeFloat(f);
+        }
+      } catch (EOFException e) {
+        //File ended do nothing.
+      } finally {
+        tempStream.close();
+        for(DataOutputStream dataOutputStream : partitions) {
+          dataOutputStream.close();
+        }
+        tempFile.delete();
+      }
+    }
+  }
+
+  public static boolean oldData(ExternalFile externalFile, String outRoot) {
+    File cdir = getCustomerOutDir(outRoot, externalFile);
+
+    if(!cdir.exists()) {
+      cdir.mkdirs();
+      return false;
+    }
+
+    String[] dirs = cdir.list();
+    for(String dir : dirs) {
+      long ldir = Long.parseLong(dir);
+      if(externalFile.timeStamp > ldir) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static String getHashDir(String customer) {
+    int bucket = customer.hashCode() % HASH_DIRS;
+    return "bucket"+bucket;
+  }
+
+  public static File getCustomerOutDir(String outRoot, ExternalFile externalFile) {
+    return new File(new File(new File(new File(outRoot), getHashDir(externalFile.customer)), externalFile.customer), externalFile.type);
+  }
+
+  public static DataOutputStream openShardOut(String outRoot, ExternalFile externalFile, String shardId, String fileName) throws IOException {
+    return new DataOutputStream(new BufferedOutputStream(new FileOutputStream(new File(getShardHome(outRoot, externalFile, shardId), fileName))));
+  }
+
+  public static File getShardHome(String outRoot, ExternalFile externalFile, String shardId) {
+    File custDir = getCustomerOutDir(outRoot, externalFile);
+    File file = new File(new File(custDir, Long.toString(externalFile.timeStamp)), shardId);
+    if(!file.exists()) {
+      file.mkdirs();
+    }
+    return file;
   }
 
   public static String getShard() {
