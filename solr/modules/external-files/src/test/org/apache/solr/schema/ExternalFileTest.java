dace0bb9054abe85dbdfe95bb92741a30742fe85
Joel Bernstein <jbernste@apache.org>
2022-09-06 13:42:33 -0400

    Add sparse index test

--- solr/modules/external-files/src/test/org/apache/solr/schema/ExternalFileTest.java
+++ solr/modules/external-files/src/test/org/apache/solr/schema/ExternalFileTest.java
@@ -146,6 +146,75 @@ public class ExternalFileTest extends SolrCloudTestCase {
   }
 
   @Test
+  public void testSparseIndex() throws Exception {
+
+    Map<String, Float> pairs = new HashMap<>();
+    Random rand = random();
+    // Generate 100 random id->float pairs
+    while(pairs.size() < 500) {
+      float f = rand.nextFloat();
+      pairs.put(Float.toHexString(f), f);
+    }
+
+    //Index those pairs.
+    UpdateRequest updateRequest = new UpdateRequest();
+    Map<String, Float> sparseIndex = new HashMap<>();
+    for(Map.Entry<String, Float> pair : pairs.entrySet()) {
+      int i = rand.nextInt();
+      if(i % 2 == 0) {
+        String id = pair.getKey();
+        String fl = pair.getValue().toString();
+        updateRequest.add("id", id, "test_f", fl);
+        sparseIndex.put(id, pair.getValue());
+      }
+    }
+
+    updateRequest.commit(cluster.getSolrClient(), COLLECTIONORALIAS);
+    String zkHost = cluster.getZkClient().getZkServerAddress();
+
+    //Construct the expected raw directories
+    File rawDirRoot = new File(cluster.getBaseDir().toFile(), "raw");
+    File dataDir = new File(new File(new File(rawDirRoot, "bucket1"), "test_ef"), String.valueOf(System.currentTimeMillis()));
+    dataDir.mkdirs();
+    File dataFile = new File(dataDir, "test_ef.txt");
+    PrintWriter out = new PrintWriter(new FileWriter(dataFile));
+    try {
+      for(Map.Entry<String, Float> pair : pairs.entrySet()) {
+        String id = pair.getKey();
+        String fl = pair.getValue().toString();
+        out.println(id+":"+fl);
+      }
+    } finally {
+      out.close();
+    }
+
+    // Process the raw file
+    File outRoot = new File(cluster.getBaseDir().toFile(), "out");
+
+    // Set the EXTERNAL_ROOT_PATH_VAR needed for the load.
+    System.setProperty(ExternalFileField2.EXTERNAL_ROOT_PATH_VAR, outRoot.getAbsolutePath());
+
+    String[] args = {rawDirRoot.getAbsolutePath(), outRoot.getAbsolutePath(), zkHost, COLLECTIONORALIAS};
+    ExternalFileUtil.main(args);
+
+    SolrParams params = params("q", "*:*", "rows", "500", "fl", "id,test_f,field(test_ef)");
+    SolrClient client = cluster.getSolrClient();
+    QueryRequest request = new QueryRequest(params);
+    QueryResponse response = request.process(client, COLLECTIONORALIAS);
+    SolrDocumentList documentList = response.getResults();
+    assertEquals(documentList.getNumFound(), sparseIndex.size());
+
+    for(int i=0; i < documentList.size(); i++) {
+      SolrDocument document = documentList.get(i);
+      String id = (String)document.getFieldValue("id");
+      float f1 = (float)document.getFieldValue("test_f");
+      float f2 = (float)document.getFieldValue("field(test_ef)");
+      assertEquals(pairs.get(id), f1, 0);
+      assertEquals(f1, f2, 0.0);
+    }
+  }
+
+  @Test
   public void testSparseExternal() throws Exception {
 
     Map<String, Float> pairs = new HashMap<>();
@@ -180,7 +249,6 @@ public class ExternalFileTest extends SolrCloudTestCase {
         if(i % 2 == 0) {
           String id = pair.getKey();
           String fl = pair.getValue().toString();
-          System.out.println("Hello:"+id + ":" + fl);
           out.println(id + ":" + fl);
           sparseMap.put(id,pair.getValue());
         }
@@ -221,11 +289,5 @@ public class ExternalFileTest extends SolrCloudTestCase {
     }
     assertEquals(s, sparseMap.size());
   }
-
-  
-
-
-
-
 
 }
